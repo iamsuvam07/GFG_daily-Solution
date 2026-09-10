5408b8a62be2fc6746d66b7558c9b43326b2738e
iamsuvam07 <iamsuvamdutta2003@gmail.com>
2026-09-10 18:36:31 +0530

    10 sep GFG problem of the day

---
+++ Pairs_with_Given_GCD_and_LCM.cpp
@@ -0,0 +1,29 @@
+class Solution {
+  public:
+    int pairCount(int x, int y) {
+        // code here
+        
+        if( y % x !=0) return 0;
+        
+        int k = y /  x;
+        
+        int distinctPrimes = 0;
+        
+        for (int i = 2; i * 1LL * i <= k; i++) {
+            if( k % i == 0) {
+                distinctPrimes++;
+                
+                while ( k % i == 0) {
+                    k /= i;
+                }
+            }
+        }
+        
+        if ( k > 1) {
+            distinctPrimes++;
+            
+        }
+        
+        return ( 1 << distinctPrimes);
+    }
+};
